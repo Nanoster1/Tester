8ad4021ba95c81a4bdcc52655225501a1f200e26
Slava <sh-e-95@mail.ru>
2021-09-12 01:11:33 +0500

    Add FileWorker

--- Tester.XUnitTests/AlgorithmTest.cs
+++ Tester.XUnitTests/AlgorithmTest.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Reflection;
 using Tester.Meta.Algorithms.FirstTask;
 using Tester.Meta.Interfaces;
+using Tester.Meta.Servis;
 using Tester.Meta.Testers;
 using Xunit;
 
@@ -13,9 +14,21 @@ namespace Tester.XUnitTests
         [Theory]
         [MemberData(nameof(AlgorithmData))]
         public void AlgorithmsTest(IAlgorithm algorithm) 
-        { 
-            var tester = new TimeTester();
-            tester.Test(algorithm);
+        {
+            var path = Path.Combine(FileWorker.AlgorithmFolder.FullName,$"text{algorithm.ToString()}.csv");
+            var list = new List<string>();
+			for (int i = 1; i <= 2000; i++)
+			{
+                TimeSpan time = TimeSpan.Zero;
+				for (int y = 0; y <= 5; y++)
+				{
+                    var tester = new TimeTester();
+                    tester.Test(algorithm);
+                    time += tester.Result;
+                }
+                list.Add($"{time / 5};");
+            }
+            File.WriteAllLines(path, list);
         }
         public static IEnumerable<object[]> AlgorithmData()
         {
