40849c9af53a4c4764a75999bfc405431008858a
Slava <sh-e-95@mail.ru>
2021-09-14 15:02:44 +0500

    Do pow algorithm

---
+++ Algorithms.FirstTask/EighthAlgorithm.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tester.Meta.Interfaces;
+
+namespace Algorithms.FirstTask
+{
+	public class EighthAlgorithm : IAlgorithm
+	{
+		public enum Methods
+		{
+			Cycle, Rec, Quick, QuickAlt
+		}
+		public string Name => nameof(EighthAlgorithm)+'1';
+
+		public double Cycle(double num, int rank)
+		{
+			if (rank == 0) return 1;
+			for(int k = 0; k < rank; k++)
+			{
+				num *= num;
+			}
+			return num;
+		} 
+
+		public double Recursion(double num, int rank)
+		{
+			if (rank == 0) return 1;
+			var result = Recursion(num, rank / 2);
+			if (rank % 2 == 1)
+				result = result * result * num;
+			else
+				result *= result;
+			return result;
+		}
+
+		public double QuickPow(double num, int rank)
+		{
+			double result;
+			if (rank % 2 == 1)
+				result = num;
+			else
+				result = 1;
+			while (rank != 0)
+			{
+				rank /= 2;
+				num *= num;
+				if (rank % 2 == 1)
+					result *= num;
+			}
+			return result;
+		}
+
+		public double QuickPowAlt(double num, int rank)
+		{
+			double result = 1;
+			while(rank != 0)
+			{
+				if (rank % 2 == 0)
+				{
+					num *= num;
+					rank %= 2;
+				}
+				else
+				{
+					result *= num;
+					rank--;
+				}
+			}
+			return result;
+		}
+
+		public void TestRun(object[] @params = null)
+		{
+			var method = (Methods)@params[2];
+			var num = (double)@params[0];
+			var rank = (int)@params[1];
+			switch (method)
+			{
+				case (Methods.Cycle):
+					Cycle(num, rank);
+					break;
+				case (Methods.Rec):
+					Recursion(num, rank);
+					break;
+				case (Methods.Quick):
+					QuickPow(num, rank);
+					break;
+				case (Methods.QuickAlt):
+					QuickPowAlt(num, rank);
+					break;
+			}
+		}
+	}
+}
--- Tester.XUnitTests/AlgorithmTest.cs
+++ Tester.XUnitTests/AlgorithmTest.cs
@@ -9,19 +9,21 @@ using Xunit;
 using System.Linq;
 using Tester.Meta.Models;
 using Tester.Meta.Algorithms.FirstTask;
+using Algorithms.FirstTask;
+using static Algorithms.FirstTask.EighthAlgorithm;
 
 namespace Tester.XUnitTests
 {
     public class AlgorithmTest
     {
-        [Theory]
+        /*[Theory]
         [MemberData(nameof(AlgorithmData))]
         public void AlgorithmsTest(IAlgorithm algorithm) 
         {
             ITester<double> tester = new TimeTester();
-            /* var enumerable = Enumerable.Range(1, 2000);
+            *//* var enumerable = Enumerable.Range(1, 2000);
              enumerable.AsParallel()
-                 .ForAll(x => tester.Test(algorithm, 5, new object[] { Vector.RandomGenerate(x) }));*/
+                 .ForAll(x => tester.Test(algorithm, 5, new object[] { Vector.RandomGenerate(x) }));*//*
             for (int i = 1; i <= 2000; i++)
             {
                 tester.Test(algorithm, 5, new object[] { Vector.RandomGenerate(i).ToArray() });
@@ -39,6 +41,18 @@ namespace Tester.XUnitTests
             yield return new object[] { new FifthAlgorithm() };
             yield return new object[] { new SixthAlgorithm() };
             yield return new object[] { new SeventhAlgorithm() };
-        }
+        }*/
+        [Fact]
+        public void TestPowAlgorithms()
+		{
+            var algorithm = new EighthAlgorithm();
+            var tester = new TimeTester();
+            var random = 10.2;
+			for (int i = 1; i <= 5; i++)
+			{
+                tester.Test(algorithm, 5, new object[] { random, i, Methods.Quick});
+			}
+            tester.Save(Environment.GetFolderPath(Environment.SpecialFolder.Desktop));
+		}
     }
 }
