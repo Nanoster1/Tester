f95f1356258bbd3daf7a715bd74c6edc0d76d370
Slava <sh-e-95@mail.ru>
2021-09-24 22:26:24 +0500

    Add exstension

---
+++ Tester.Meta/Models/MatrixExstention.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tester.Meta.Models
+{
+	public static class MatrixExstention
+	{
+		public static Matrix ToMatrix(this int[][] enumerable)
+		{
+			return new Matrix(enumerable);
+		}
+	}
+}
