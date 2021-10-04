742fbf9a9dbe481c6ed44b5b829680771532cc06
Slava <sh-e-95@mail.ru>
2021-10-04 20:47:55 +0500

    Add Fibonachi

---
+++ SlavaAlgorithms/Library.fs
@@ -0,0 +1,20 @@
+﻿namespace SlavaAlgorithms
+
+module Fibonachi =
+    let LazyAlg num = 
+        seq{
+            let mutable index = 0
+            
+            let mutable num = 0
+            if index <= num then
+                yield num
+            let mutable nextnum = 1
+            if index <= num then
+                yield nextnum
+            while index <= num do 
+                let saveNum = nextnum                                        
+                nextnum <- num + nextnum    
+                num <- saveNum
+                yield nextnum
+            }        
+        
