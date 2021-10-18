237be53fc2bface2f879b216cbc52a4b543eba05
Nanoster <danil2002.11@mail.ru>
2021-10-19 01:03:34 +0500

    HanTowers

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,9 +1,9 @@
-﻿using Algorithms.FirstTask;
-using System;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using Algorithms.FirstTask.FirstTask;
-using Tester.DataTypes;
 using Tester.Meta.Interfaces;
 using Tester.Meta.Testers;
 using Tester.Meta.Models;
@@ -13,23 +13,32 @@ namespace ConsoleTest
 {
     class Program
     {
+        static void Relocate(Stack<byte> start, Stack<byte> temp, Stack<byte>end, int elements)
+        {
+            if (elements == 0) return;
+            Relocate(start, end, temp,elements - 1);
+            end.Push(start.Pop());
+            Collect(temp, start, end, elements - 1);
+        }
+
+        static void Collect(Stack<byte> start, Stack<byte> temp, Stack<byte>end, int elements)
+        {
+            if (elements == 0) return;
+            Relocate(start, end, temp,elements - 1);
+            end.Push(start.Pop());
+            Collect(temp, start, end, elements - 1);
+        }
+        
         static void Main(string[] args)
         {
-            var tester = new TimeTester();
-            var tester1 = new MemoryTester();
-
-            for (int i = 2; i < 300; i++)
-			{
-                var graph = Graph<uint>.GetRandomGraph(i);
-                var node = graph.Nodes.First();
-                var node2 = graph.Nodes.Last();
-                tester.Test(() => GraphShortestPath.AlgortighmFloydWarshall(graph,node,node2), 5, nameof(Fibonachi.LazyAlg));
-                tester1.Test(() => GraphShortestPath.AlgortighmFloydWarshall(graph, node, node2), 5, nameof(Fibonachi.LazyAlg));
+            var start = new Stack<byte>(Enumerable.Range(0, 4).Select(x => (byte)x));
+            var temp = new Stack<byte>();
+            var end = new Stack<byte>();
+            Relocate(start, temp, end, 4);
+            foreach (var i in end)
+            {
+                Console.Write(i);
             }
-            tester.SaveAsExcel(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), nameof(TimeTester));
-            tester.AllResults.Clear();
-            tester1.SaveAsExcel(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), nameof(MemoryTester));
-            tester1.AllResults.Clear();
         }
     }
 }
