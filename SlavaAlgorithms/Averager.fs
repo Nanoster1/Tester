066cbd2443bc1b62becdf6c47001ae093c6af035
Slava <sh-e-95@mail.ru>
2021-11-08 22:24:44 +0500

    add alg

---
+++ SlavaAlgorithms/Averager.fs
@@ -0,0 +1,16 @@
+﻿namespace SlavaAlgorithms
+
+open System
+open System.Collections.Generic
+
+type Averager(countFunction:Func<float>, averageDegree) =
+    let queue = new Queue<float>()
+    let mutable sum = 0.0;
+    member this.Measure() =        
+        let value = countFunction.Invoke()
+        queue.Enqueue value
+        sum <- sum + value
+        if queue.Count > averageDegree then
+            sum <- sum - queue.Dequeue()
+        sum / float queue.Count
+
