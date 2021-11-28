b429196b82c046cfec044c09b7eef81e651cecb8
goroshek <yulyagroshenko@mail.ru>
2021-11-28 22:27:14 +0500

    add overloads merge sort

--- Algorithms/FirstTask/MergeSort.cs
+++ Algorithms/FirstTask/MergeSort.cs
@@ -63,5 +63,60 @@ namespace Algorithms.FirstTask
             }
             return result;
         }
+        public static char[] Sort(char[] arr)
+        {
+            if (arr.Length > 1)
+            {
+                var left = new char[arr.Length / 2];
+                var right = new char[arr.Length - left.Length];
+                for (int i = 0; i < left.Length; i++)
+                {
+                    left[i] = arr[i];
+                }
+                for (int j = 0; j < right.Length; j++)
+                {
+                    right[j] = arr[left.Length + j];
+                }
+                if (left.Length > 1)
+                    left = Sort(left);
+                if (right.Length > 1)
+                    right = Sort(right);
+                arr = Merge(left, right);
+                return arr;
+            }
+            else
+            {
+                return arr;
+            }
+        }
+        private static char[] Merge(char[] left, char[] right)
+        {
+            var result = new char[left.Length + right.Length];
+            int i = 0, j = 0;
+            for (int k = 0; k < result.Length; k++)
+            {
+                if (i > left.Length - 1)
+                {
+                    result[k] = right[j];
+                    j++;
+                }
+                else if (j > right.Length - 1)
+                {
+                    result[k] = left[i];
+                    i++;
+                }
+                else if (left[i] >= right[j])
+                {
+                    result[k] = right[j];
+                    j++;
+                }
+                else
+                {
+                    result[k] = left[i];
+                    i++;
+                }
+            }
+            return result;
+        }
     }
 }
